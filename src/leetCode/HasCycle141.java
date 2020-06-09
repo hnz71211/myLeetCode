63614906722627cc85e3c7a0995c6adf7ce7262c
hexli <hxl71396812@gmail.com>
2020-06-09 11:26:25 +0800

    双指针：环形链表，环形检测

--- src/leetCode/HasCycle141.java
+++ src/leetCode/HasCycle141.java
@@ -21,12 +21,3 @@ public class HasCycle141 {
   }
 
 }
-
-class ListNode {
-  int val;
-  ListNode next;
-  ListNode(int x) {
-    val = x;
-    next = null;
-  }
-}
---
+++ src/leetCode/ListNode.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+/**
+ * @ClassName: ListNode
+ * @Description:
+ * @Author: hexli
+ * @Date: 2020/6/9
+ **/
+class ListNode {
+  int val;
+  ListNode next;
+  ListNode(int x) {
+    val = x;
+    next = null;
+  }
+}
