a7edd0623c09a75d8bf572a941c04c5ba63a4a38
hexli <hxl71396812@gmail.com>
2020-06-11 17:24:53 +0800

    最长增长子序列

--- src/dynamic_programming/lis/LongestIncreasingSubsequence.java
+++ src/dynamic_programming/lis/LongestIncreasingSubsequence.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 /**
  * @ClassName: LongestIncreasingSubsequence
- * @Description:
+ * @Description: 最长增长子序列
+ *    dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
  * @Author: hexli
  * @Date: 2020/5/20
  **/
@@ -12,14 +13,17 @@ public class LongestIncreasingSubsequence {
 
   public static int lengthOfLIS(int[] nums) {
 
+    // dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
     int[] dp = new int[nums.length];
     Arrays.fill(dp, 1);
 
     for (int i = 0; i < nums.length; i++) {
       for (int j = 0; j < i; j++) {
         if (nums[i] > nums[j]) {
-          // 假设 dp[0...i-1] 都已知，想办法求出 dp[i]
-          dp[i] = Math.max(dp[i], dp[j] + 1);
+          dp[i] = Math.max(
+                  dp[i],
+                  dp[j] + 1
+          );
         }
       }
     }
